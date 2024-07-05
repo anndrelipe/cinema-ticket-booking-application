0e965325a762ad83fdba37baec6059ef62bbf53d
anndrelipe <ppeixoto.andre@gmail.com>
2024-07-04 22:01:34 -0300

    Adiciona Controller para movies

---
+++ backend/src/controllers/moviesController.js
@@ -0,0 +1,16 @@
+import Movies from "../models/MoviesModel.js";
+
+class movieController {
+    static async getAllMovies (req, res) {
+        try {
+            const listaFilmes = await Movies.find({});
+            res.status(200).json({status : 200, content : listaFilmes})
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({status : 500, message : "Erro ao tentar adquirir filmes no banco de dados."});
+            return
+        }
+    }
+}
+
+export default movieController
