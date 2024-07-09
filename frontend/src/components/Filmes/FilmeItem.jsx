ec42bbf0766eec19809e3db71d67f97736308e40
Thiago Elias <contatothiagoelias22@gmail.com>
2024-07-09 11:12:33 -0300

    Clicar em cada filme agora leva para uma segunda pagina

--- frontend/src/components/Filmes/FilmeItem.jsx
+++ frontend/src/components/Filmes/FilmeItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom"
 
 export function FilmeItem({ item, onMouseOver }) {
   return (
     <div className='Filme-Item'>
+      <Link to="/pagina2">
       {item.media && <img src={item.media.img1} onMouseOver={onMouseOver} alt="Filme" />}
+      </Link>
     </div>
   );
 }
