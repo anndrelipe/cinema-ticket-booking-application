d344bed0ab5eed3afd8c7de0e8ef91c701310a16
Thiago Elias <contatothiagoelias22@gmail.com>
2024-07-11 01:49:59 -0300

    Criado e adaptado a pagina de Sessões estática por agora, posteriormente alimentada usando da api para sessões

--- frontend/src/Pages/Sessoes/SessaoItem.jsx
+++ frontend/src/Pages/Sessoes/SessaoItem.jsx
@@ -1,5 +1,70 @@
+import { Popcorn } from "lucide-react"
+
 export function SessaoItem() {
   return (
-      <h1>SessãoItem</h1>
+      <div className="SessaoItem">
+        <div className="SessaoItem-Cinema">
+            <Popcorn className="popcorn-icon"/>
+            <div className="SessaoItem-Cinema-text">
+              <h3 className="SessaoItem-Cinema-End">Av. Washington Soares, 85 - Edson Queiroz, Fortaleza</h3>
+              <h2 className="SessaoItem-Cinema-Nome">Cinema Iguatemi</h2>
+            </div>
+          </div>
+
+          <div className="SessaoItem-divisao"></div>
+
+          <div className="SessaoItem-Sessoes">
+            <div className="SessaoItem-SessoesCategs">
+
+              <div className="SessaoItem-SessoesCategs-Categoria">
+                Legendado</div>
+
+              <div className="SessaoItem-SessoesCategs-Horarios">
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  21:00
+                </div>
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  21:45
+                </div>
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  22:00
+                </div>
+              </div>
+
+            </div>
+
+            <div className="SessaoItem-SessoesCategs">
+              <div className="SessaoItem-SessoesCategs-Categoria">
+                Vip Dublado</div>
+              <div className="SessaoItem-SessoesCategs-Horarios">
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  21:00
+                </div>
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  21:45
+                </div>
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  22:00
+                </div>
+              </div>
+            </div>
+
+            <div className="SessaoItem-SessoesCategs">
+              <div className="SessaoItem-SessoesCategs-Categoria">
+                Dublado</div>
+              <div className="SessaoItem-SessoesCategs-Horarios">
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  21:00
+                </div>
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  21:45
+                </div>
+                <div className="SessaoItem-SessoesCategs-Horario">
+                  22:00
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
   )
 }
--- frontend/src/Pages/Sessoes/Sessoes.jsx
+++ frontend/src/Pages/Sessoes/Sessoes.jsx
@@ -3,6 +3,9 @@ import "./styles.css";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { SessaoItem } from './SessaoItem';
 import { FilmeDetalhes } from './FilmeDetalhes';
+import { Conteudo } from "../../components/Conteudo/Conteudo";
+import { Footer } from "../../components/Footer/Footer";
+import { DiasSessoes } from "./DiasSessoes";
 
 export function Sessoes() {
 
@@ -12,9 +15,17 @@ export function Sessoes() {
       
       <FilmeDetalhes/>
 
-      <SessaoItem/>
-      <SessaoItem/>
-      <SessaoItem/>
+
+      <Conteudo style="overflow: hidden">
+        
+        <DiasSessoes/>
+
+        <SessaoItem/>
+        <SessaoItem/>
+        <SessaoItem/>
+      </Conteudo>
+
+      <Footer/>
     </>
   );
 }
